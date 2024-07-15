f257ae7147dde7a4add14bb4047fcd83c8dda91d
Deppfellow <raid.rafif3@gmail.com>
2024-07-15 13:55:28 +0700

    chore: add not found pages and routes to *

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,13 +2,16 @@ import ifritSign from '@/assets/ifritSign.webp';
 
 function NotFound() {
 	return (
-		<div className="flex h-screen items-center justify-center bg-gradient-to-br from-slate-50 to-slate-200">
+		<div className="to-slate-150 relative flex h-screen bg-gradient-to-br from-slate-50">
 			<img
 				src={ifritSign}
 				alt="NotFoundSign"
-				style={{ width: '60vw' }}
-				className="inset-0 block"
+				// style={{ width: '60vw' }}
+				className="fixed left-1/4 top-1/4 z-0 w-[50vw] translate-x-[-80px] translate-y-[-20px]"
 			/>
+			<div className="z-50 flex h-full w-full items-center justify-center">
+				<h1 className="text-4xl font-bold">404 - Not Found</h1>
+			</div>
 		</div>
 	);
 }
