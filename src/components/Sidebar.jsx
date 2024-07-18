284433c0e174d50441c2951b5897b7966c487c25
Deppfellow <raid.rafif3@gmail.com>
2024-07-18 15:47:26 +0700

    chore: add font-family style into index.css and restyle the app title

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ export const Context = React.createContext();
 function Sidebar({ addNewProject, value, projects }) {
 	return (
 		<div className="sidebar w-3/12 min-w-48 border border-gray-700 bg-gray-300">
-			<h1 className="text-bold mx-4 my-3 text-4xl">SEPIK</h1>
+			<h1 className="text-bold mx-4 my-3 text-4xl font-bold">SEPIK</h1>
 			<ProjectForm addNewProject={addNewProject} />
 			<Context.Provider value={value}>
 				<ProjectList projects={projects} />
