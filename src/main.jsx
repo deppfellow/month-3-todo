193c83adf7d691acc40b76a8c40af3dc518b1c78
Deppfellow <raid.rafif3@gmail.com>
2024-07-21 17:35:10 +0700

    refactor: Reconfig react router using recommended way. Then add button back to home in NotFound Page.

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+// import App from './App.jsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from '@/pages/Home';
+import NotFound from '@/pages/NotFound.jsx';
+
+const router = createBrowserRouter([
+	{ path: '/', element: <Home />, errorElement: <NotFound /> },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+import { Button } from '@/components/ui/button';
 import ifritSign from '@/assets/ifritSign.webp';
 
 function NotFound() {
@@ -9,8 +11,11 @@ function NotFound() {
 				// style={{ width: '60vw' }}
 				className="fixed left-1/4 top-1/4 z-0 w-[50vw] translate-x-[-80px] translate-y-[-20px]"
 			/>
-			<div className="z-50 flex h-full w-full items-center justify-center">
+			<div className="z-50 flex h-full w-full flex-col items-center justify-center gap-2">
 				<h1 className="text-4xl font-bold">404 - Not Found</h1>
+				<Link to={'/'}>
+					<Button size="sm">Back to Home</Button>
+				</Link>
 			</div>
 		</div>
 	);
