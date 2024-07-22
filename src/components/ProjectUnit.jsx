492e9f3f745995cc731a2c47c25e9256e458517e
Deppfellow <raid.rafif3@gmail.com>
2024-07-22 13:12:03 +0700

    style: additional conditional rendering in project list to make button disabled when project is active

--- src/components/ProjectUnit.jsx
+++ src/components/ProjectUnit.jsx
@@ -1,12 +1,24 @@
 import { useContext } from 'react';
 import { Context } from '@/components/Sidebar';
+import { Button } from '@/components/ui/button';
 
 function ProjectUnit({ id, title }) {
 	const [activeProject, setActiveProject] = useContext(Context);
 
 	return (
-		<li className="text-base">
-			<button onClick={() => setActiveProject(id)}>{title}</button>
+		<li className="flex flex-col text-base">
+			{activeProject === id ? (
+				<Button size="xs" disabled>
+					{title}
+				</Button>
+			) : (
+				<Button size="xs" onClick={() => setActiveProject(id)}>
+					{title}
+				</Button>
+			)}
+			{/* <Button size="xs" onClick={() => setActiveProject(id)}>
+				{title}
+			</Button> */}
 		</li>
 	);
 }
